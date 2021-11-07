9a48aafcd6bbd93f7e65e1f7cd5a703a462831ee
MilesChou <jangconan@gmail.com>
2021-11-07 14:48:51 +0800

    The second year

--- helloworld.php
+++ helloworld.php
@@ -1,11 +1,28 @@
 <?php
 
+/**
+ * Hello world class
+ *
+ * Used to display the phrase "Hello World!" in a console.
+ *
+ * @author Miles
+ */
 class HelloWorld
 {
+    /**
+     * The phrase to display in the console
+     */
+    public const PHRASE = 'Hello World!';
+
+    /**
+     * Main method
+     *
+     * @return void
+     */
     public static function main(): void
     {
-        // Displays "Hello World!" on the console.
-        echo 'Hello World!';
+        // Displays our phrase in a console.
+        echo self::PHRASE;
     }
 }
 
