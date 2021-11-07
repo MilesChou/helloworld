c034a9270aa8f6d279587cc89948f993c125cbda
MilesChou <jangconan@gmail.com>
2021-11-07 14:56:19 +0800

    The third year

--- helloworld.php
+++ helloworld.php
@@ -6,23 +6,60 @@
  * Used to display the phrase "Hello World!" in a console.
  *
  * @author Miles
+ * @license MIT
+ * @version 1.2
+ * @see echo
+ * @see README
+ * @todo Create factory methods
+ * @link https://github.com/MilesChou/helloworld
  */
 class HelloWorld
 {
     /**
-     * The phrase to display in the console
+     * The default phrase to display in the console
      */
     public const PHRASE = 'Hello World!';
+
+    /**
+     * The phrase to display in the console
+     */
+    private $hello_world;
+
+    /**
+     * Constructor
+     *
+     * @param string $hw The phrase to display in the console
+     */
+    public function __construct(string $hw)
+    {
+        $this->hello_world = $hw;
+    }
+
+    /**
+     * Display the phrase "Hello World!" in a console
+     *
+     * @return void
+     */
+    public function sayPhrase(): void
+    {
+        // Displays our phrase in a console.
+        echo $this->hello_world;
+    }
 
     /**
      * Main method
      *
      * @return void
      */
-    public static function main(): void
+    public static function main()
     {
-        // Displays our phrase in a console.
-        echo self::PHRASE;
+        $hw = new self(self::PHRASE);
+
+        try {
+            $hw->sayPhrase();
+        } catch (Exception $e) {
+            // Do nothing!
+        }
     }
 }
 
